bf5659d4d7313ae7db0fcb2c7207500aaac0cbbb
tanvirtbf <tanver018765@gmail.com>
2024-12-08 11:13:05 +0600

    start module.exports and exports and setup basic code

---
+++ module.exports VS exports/app.js
@@ -0,0 +1,23 @@
+const math = require('./math.js')
+
+const user = {
+  name: 'Tanvir Ahmed',
+  age : 25, 
+  address: {
+    city: 'Dhaka',
+    state: 'Sonir Akhra'
+  },
+  hobbies: ['Teaching', 'Coding', 'Football']
+}
+
+let address = user.address
+
+address.pinCode = 6064
+address.country = 'Bangladesh'
+
+
+
+
+
+
+
---
+++ module.exports VS exports/math.js
@@ -0,0 +1,13 @@
+function sum(...nums){
+  return nums.reduce((curr,acc) => curr + acc)
+}
+
+function product(...nums){
+  return nums.reduce((curr,acc) => curr * acc)
+}
+ 
+console.log(module.exports === exports) // true
+// ekhane jodio true ashce kintu tao onk difference ase 
+
+module.exports.sum = sum
+module.exports.product = product
