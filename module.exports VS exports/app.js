8bf0fe298632d596efcb66fd9063b11edc61caef
tanvirtbf <tanver018765@gmail.com>
2024-12-08 11:30:25 +0600

    Example 1 complete

--- module.exports VS exports/app.js
+++ module.exports VS exports/app.js
@@ -1,5 +1,6 @@
 const math = require('./math.js')
 
+// Example 1: 
 const user = {
   name: 'Tanvir Ahmed',
   age : 25, 
@@ -9,11 +10,13 @@ const user = {
   },
   hobbies: ['Teaching', 'Coding', 'Football']
 }
-
 let address = user.address
-
+console.log(user.address === address)
 address.pinCode = 6064
 address.country = 'Bangladesh'
+
+// Example 1 er moddhe user nam e ekta object ase jeta ke amra module object dhorte pari and user object er moddhe address nam e ekta object ase setake amra exports object dhorte pari . to behind the scene let exports = module.exports kora hoise jei karone exports and module.exports same memory te kaj kore . jemon upore let address = user.address korar por jokhon address.pinCode and address.country dara address er value change korle seta main user object e reflect hocche karon object holo non-premitive data . same concept hoise module.exports and exports er moddhe....
+
 
 
 
