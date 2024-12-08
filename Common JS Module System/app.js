45abdb2d612484b4e449d7fb2e3159c8a8a80de9
tanvirtbf <tanver018765@gmail.com>
2024-12-08 09:28:02 +0600

    2 function create

--- Common JS Module System/app.js
+++ Common JS Module System/app.js
@@ -1,3 +1,10 @@
+
 function sum(...nums){
-  console.log("Hello")
+  return nums.reduce((curr,acc) => curr + acc)
 }
+
+function product(...nums){
+  return nums.reduce((curr,acc) => curr * acc)
+}
+
+console.log(sum(1,2,3,4,5))
