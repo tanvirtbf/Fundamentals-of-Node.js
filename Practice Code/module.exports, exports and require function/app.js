7c037751ba8e2eea71fca2b8181dbcb22e318faa
tanvirtbf <tanver018765@gmail.com>
2024-12-08 14:21:13 +0600

    complete practice code

--- Practice Code/module.exports, exports and require function/app.js
+++ Practice Code/module.exports, exports and require function/app.js
@@ -7,3 +7,23 @@ console.log(sum(1,2,3,4,5))
 console.log(sub(6,2))
 console.log(mul(3,4,2))
 console.log(div(6,1))
+
+const returnString = require('./returnString')
+console.log(returnString) // Hello World!
+
+const returnNumber = require('./returnNumber')
+console.log(returnNumber) // 123
+
+const returnEmptyObject = require('./returnEmptyObject')
+console.log(returnEmptyObject) // {}
+
+const returnBool = require('./returnBool')
+console.log(returnBool)
+
+// const exportsHello = require('./exports')
+// const {hello} = require('./exports')
+// console.log(exportsHello.hello())
+// console.log(hello())
+
+const exportsHello = require('./exports')
+console.log(exportsHello)
---
+++ Practice Code/module.exports, exports and require function/exports.js
@@ -0,0 +1,10 @@
+function hello(){
+  return 'Hello World!'
+}
+
+// exports.hello = hello
+
+// let send = module.exports 
+// send.hello = hello
+
+exports = hello // empty object return hobe karon assignment operator '=' diye exports er moddhe new value set kora hoise
