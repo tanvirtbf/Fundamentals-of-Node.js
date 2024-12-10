8e6e6e894b89d1ed7edaba944616970a356de70f
tanvirtbf <tanver018765@gmail.com>
2024-12-10 09:40:01 +0600

    start creating custom require function

---
+++ Custom Require Function/app.js
@@ -0,0 +1,9 @@
+const b = loadModule('./math.js')
+
+function loadModule(path){
+  const fs = require('fs')
+  const fileContent = fs.readFileSync(path).toString()
+  console.log(fileContent)
+}
+
+console.log(b)
